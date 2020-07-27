785a44c0c1c108c883531c3d300d0fda2b323b28
Christine Fletcher <chrismis79@gmail.com>
2020-07-27 13:14:40 -0500

    initial set up complete, background inmage and grid made

---
+++ game/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Grid from './components/Grid'
+import './App.css';
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.speed = 100;
+    this.rows = 30;
+    this.cols = 50;
+
+    this.state = {
+      generation: 0,
+      gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>John Conway's Game of Life</h1>
+        <Grid
+          gridFull={this.state.gridFull}
+          rows={this.rows}
+          cols={this.cols}
+          selectBox={this.selectBox}
+        />
+        <h2>Generations: {this.state.generation}</h2>
+      </div>
+    )
+  }
+}
+
+
+export default App;
---
+++ game/src/components/Grid.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Box from './Box'
+
+class Grid extends Component {
+
+    render() {
+        let width = this.props.cols * 14;
+        let rowsArr = [];
+        let boxClass = " ";
+        for (let i = 0; i < this.props.rows; i++) {
+            for (let j = 0; j < this.props.cols; j++) {
+                let boxid = i + "_" + j;
+
+                boxClass = this.props.gridFull[i][j] ? 'box on' : 'box off';
+                rowsArr.push(
+                    <Box
+                        boxClass={boxClass}
+                        key={boxid}
+                        boxid={boxid}
+                        row={i}
+                        col={j}
+                        selectBox={this.props.selectBox}
+                    />
+                )
+            }
+        }
+
+
+
+        return (
+            <div className="grid" style={{ width: width }}>
+                {rowsArr}
+            </div>
+        );
+    }
+}
+
+export default Grid;
+
